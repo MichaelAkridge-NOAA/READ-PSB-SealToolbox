670538be17116017180dbff5f29b321a046827b0
ejosephson <elizabeth.josephson@noaa.gov>
2024-05-15 13:21:35 -0500

    Production2

---
+++ Production2/scripts/counts1415.R
@@ -0,0 +1,36 @@
+#load some libraries
+library(leaflet)
+library(leafpop)
+library(dplyr)
+library(leaflet.minicharts)
+library(lubridate)
+
+
+######################
+
+
+
+
+counts1415<-read.csv("./data/countsummary20142015.csv")
+
+
+counts1415$SURVEYDATE <- mdy(counts1415$SURVEYDATE) - days(1)
+
+
+tilesURL <- "http://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+
+basemap <- leaflet(width = "100%", height = "600px") %>%
+ # addTiles(tilesURL) 
+addProviderTiles(providers$Esri.WorldImagery)
+colors <- c("#3093e5", "#fcba50")
+basemap %>%
+  addMinicharts(
+    counts1415$LON, counts1415$LAT,
+    chartdata = counts1415[, c("HG", "PV")],
+    type = "pie",
+    time = counts1415$SURVEYDATE,
+    colorPalette = colors,
+    width = 170 * sqrt(counts1415$TOTALSEALS) / sqrt(max(counts1415$TOTALSEALS)), transitionTime = 0,
+    popup = popupArgs(
+      labels = c("Gray Seals", "Harbor Seals"))
+  )
---
+++ Production2/scripts/global.R
@@ -0,0 +1,18 @@
+##global file 
+
+#
+
+library (shiny)
+library(DT)
+library(data.table)
+library(plyr)
+library(dplyr)
+library(sp)
+library (rgdal)
+library(shinydashboard)
+library(leaflet)
+library(DBI)
+library(htmltools)
+library(sf)
+library(crosstalk)
+library(xtable)
