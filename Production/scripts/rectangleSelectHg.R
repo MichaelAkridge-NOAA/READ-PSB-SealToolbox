33084651a01eebc615ebfe16638a277f636e555c
ejosephson <elizabeth.josephson@noaa.gov>
2024-05-16 15:28:58 -0500

    Hg page stripped down - no map no dt

--- Production/scripts/rectangleSelectHg.R
+++ Production/scripts/rectangleSelectHg.R
@@ -36,19 +36,19 @@ shared_cts <- SharedData$new(data3, key = ~Site, group = "shared_obj")
 #   )),
 # )  
   
-ltlf5<- leaflet(shared_sites) %>% 
-  #addTiles(tilesURL) %>%
-  addProviderTiles(providers$Esri.WorldImagery) %>%
-  addCircleMarkers(
-    lng = ~LON,
-    lat = ~LAT,
-  #  group = ~HAULOUTSITECODE,popup = ~paste(Site, '   <br/>  ',
-  #                                      HAULOUTAREA,'   <br/>  ' ),
-    group = ~HAULOUTSITECODE,popup = ~paste(Site),
-    radius = 3,
-    color = 'yellow'
-    
-  )   
+# ltlf5<- leaflet(shared_sites) %>% 
+#   #addTiles(tilesURL) %>%
+#   addProviderTiles(providers$Esri.WorldImagery) %>%
+#   addCircleMarkers(
+#     lng = ~LON,
+#     lat = ~LAT,
+#   #  group = ~HAULOUTSITECODE,popup = ~paste(Site, '   <br/>  ',
+#   #                                      HAULOUTAREA,'   <br/>  ' ),
+#     group = ~HAULOUTSITECODE,popup = ~paste(Site),
+#     radius = 3,
+#     color = 'yellow'
+#     
+#   )   
 
 
 
@@ -62,17 +62,18 @@ body <-  dashboardBody( tags$head(tags$style(HTML('
                tags$head(tags$link(rel = "icon", type = "image/png", href = "favicon.png")
                )
     ),
-  ),
-  fluidRow(
-
-    column(6,ltlf5, h4(style="text-align: justify;","The NEFSC has been conducting aerial surveys of gray seal pupping colonies since 2005 to monitor the number of pups born in U.S. waters. 
-                       Surveys were flown from either a Cessna 252, Cessna Skymaster, or NOAA Twin Otter at altitudes ranging from 180-230 m, 
-                       using hand-held or belly-mounted Canon camera systems. Ideally, surveys are flown after all pups are born and before they start leaving the colony, 
-                       but in some years surveys may have been flown before or after this optimal window. Furthermore, not all colonies may have been surveyed each year 
-                       due to logistics or if the site was not yet an established pupping colony. This map represents the minimum count of pups born on each colony from 2005-2021, 
-                       and if multiple flights were flown in a season, the maximum count of replicate surveys. For more information, see Wood et al.", tags$a(href="https://academic.oup.com/jmammal/article/101/1/121/56750962020", "2020,"), tags$a(href="https://repository.library.noaa.gov/view/noaa/46455", "2022.")), 
-                       h4("Use the bracket tool in the corner of this map and drag the corners to select rookeries (blue dots) of interest. 
-                       Or just use the filters in the datatable at left to select data."))
+#  )
+# ,
+#   fluidRow(
+# 
+#     column(6,ltlf5, h4(style="text-align: justify;","The NEFSC has been conducting aerial surveys of gray seal pupping colonies since 2005 to monitor the number of pups born in U.S. waters. 
+#                        Surveys were flown from either a Cessna 252, Cessna Skymaster, or NOAA Twin Otter at altitudes ranging from 180-230 m, 
+#                        using hand-held or belly-mounted Canon camera systems. Ideally, surveys are flown after all pups are born and before they start leaving the colony, 
+#                        but in some years surveys may have been flown before or after this optimal window. Furthermore, not all colonies may have been surveyed each year 
+#                        due to logistics or if the site was not yet an established pupping colony. This map represents the minimum count of pups born on each colony from 2005-2021, 
+#                        and if multiple flights were flown in a season, the maximum count of replicate surveys. For more information, see Wood et al.", tags$a(href="https://academic.oup.com/jmammal/article/101/1/121/56750962020", "2020,"), tags$a(href="https://repository.library.noaa.gov/view/noaa/46455", "2022.")), 
+#                        h4("Use the bracket tool in the corner of this map and drag the corners to select rookeries (blue dots) of interest. 
+#                        Or just use the filters in the datatable at left to select data."))
  #   ,
    
     
