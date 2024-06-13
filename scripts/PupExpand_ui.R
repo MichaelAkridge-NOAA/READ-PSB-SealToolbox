da3d19f3a8798e21b603e5533d4e9916ce990a96
ejosephson <elizabeth.josephson@noaa.gov>
2024-06-13 09:57:51 -0500

    removed non-production scripts

--- scripts/PupExpand_ui.R
+++
@@ -1,9 +0,0 @@
-ui <- fluidPage(# Application title
-  titlePanel("Pup Counts"),
-  mainPanel(
-    tabsetPanel(
-      tabPanel("Gray Seals", dataTableOutput("my_table"))
-      ,
-      tabPanel("Harbor Seals", dataTableOutput("my_table2"))
-    )
-  ))
